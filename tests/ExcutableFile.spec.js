e39396677b5b026f15afe7ccf529d11c07538b39
JOSHUA-BRONI <149646634+JOSHUA-BRONI@users.noreply.github.com>
2023-11-08 13:36:49 +0000

    Update ExcutableFile.spec.js

--- tests/ExcutableFile.spec.js
+++ tests/ExcutableFile.spec.js
@@ -1,30 +1,38 @@
-#!/usr/bin/env node
+import { test, expect } from '@playwright/test';
 
-import { test, expect } from "@playwright/test";
- 
-test("Check all Hubtel for Hospitals links excluding links with / or wrong format", async ({
-  page,
-}) => {
+test('Check all Hubtel for Hospitals links excluding links with / or wrong format', async ({ page }) => {
   // Navigate to Hubtel for hospitals page
-  await page.goto("https://explore.hubtel.com/hospitals/");
- 
+  await page.goto('https://explore.hubtel.com/hospitals/');
+
   // Find and collect all anchor elements on the page
-  const links = await page.$$eval("a", (elements) =>
+  const links = await page.$$eval('a', (elements) =>
     elements.map((element) => {
-      const href = element.getAttribute("href");
+      const href = element.getAttribute('href');
+      // Check if the href is empty or a fragment.
+      if (!href || href.startsWith('#')) {
+        return "";
+      }
       return href;
     })
   );
- 
+
   // Loop through each link and check if it's working
   for (const link of links) {
-    if (link.startsWith("http://") || link.startsWith("https://")) {
-      const response = await page.goto(link, { timeout: 3000 * 10 });
- 
-      const statusCode = await response.status();
- 
-      expect(statusCode).toBe(200);
+    try {
+      if (link) {
+        if (link === '/' || link.startsWith('http://') || link.startsWith('https://')) {
+          console.log(`link : ${link}`);
+        } else {
+          await page.goto(link);
+          await page.waitForNavigation(); // Wait for the navigation to complete
+          const statusCode = page.lastResponse()?.status();
+          expect(statusCode).toBe(200);
+
+        }
+      }
+    } catch (error) {
+      // Log the error if navigation fails
+      console.error(`Failed to navigate to ${link}: ${error}`);
     }
   }
 });
- 
